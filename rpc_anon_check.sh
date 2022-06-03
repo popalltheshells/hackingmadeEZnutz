7c76f9e177fef1f680dc820920435911958c2ebc
Iggy <ignatiusasikinn@gmail.com>
2022-06-03 11:29:24 -0400

    Add files via upload

---
+++ rpc_anon_check.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+a=0
+while [ $a -le 254 ]
+do
+   echo "trying" $a
+   #get list of anonymous RPC access from a subnet. Change the IP manually tho -- too lazy to make this script asks for inputs.
+   rpcclient -U '' -N 10.0.0.$a 2>&1
+   #get list of anonymous RPC access to enumerate domain users
+   #rpcclient -U '' -N 10.0.0.$a -c enumdomusers 2>&1
+   ((a++))
+done
---
+++ smb_shares_check.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+a=0
+while [ $a -le 254 ]
+do
+   #Check for anonymous SMB access to the following shares. populate the Username and Password if you have domain cred to see if they have access to the sha>
+   #sample for domain user: smbclient //10.x.x.$a/IPC$ U "username"%"password" 2>&1
+   #otherwise anonymous checks will be done.
+
+   echo "trying" $a
+   smbclient //10.x.x.$a/IPC$ U " "%" " 2>&1
+   smbclient //10.240.20.$a/C$ U " "%" " 2>&1
+   smbclient //10.x.x.$a/Public U " "%" " 2>&1
+   smbclient //10.240.20.$a/Admin$ U " "%" " 2>&1
+
+   ((a++))
+done
